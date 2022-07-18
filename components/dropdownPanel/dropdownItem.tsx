d6ebb99b6de6e64e3508fcf94af02386639a7f30
Ievhenii <youn4ik@gmail.com>
2022-07-18 18:51:26 +0300

    added search dropdown

---
+++ components/dropdownPanel/dropdownItem.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../styles/DropdownItem.module.scss";
+
+interface IDropdownItem {
+  item: string;
+  onClick: (arg: string) => string;
+}
+const DropdownItem = ({ item, onClick }: IDropdownItem) => {
+  return <div className={styles.dropdownItem} onClick={() => onClick(item)}>{item}</div>;
+};
+
+export default DropdownItem;
